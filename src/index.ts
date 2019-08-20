7f4bc4d034c52b7c72c5ff400738dd6bf12e81d7
9aoyang <9aoyang@gmail.com>
2019-08-20 09:20:33 +0800

    feat: define send method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-function axios(config) {}
+import { AxiosRequestConfig } from './types/index'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig) {
+  // TODO
+}
 
 export default axios
