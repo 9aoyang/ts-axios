0be849c1e05d54ddf44e41c139760112a0b133d0
9aoyang <9aoyang@gmail.com>
2019-08-20 14:52:02 +0800

    feat: handle request url params

--- examples/server.js
+++ examples/server.js
@@ -32,6 +32,11 @@ router.get('/simple/get', function (req, res) {
     msg: 'hello world'
   })
 })
+
+router.get('/base/get', function (req, res) {
+  res.json(req.query)
+})
+
 app.use(router)
 
 const port = process.env.PORT || 8080
