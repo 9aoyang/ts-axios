46a376b58b8fecf2bf945b164dc8c515bb540547
9aoyang <9aoyang@gmail.com>
2019-08-21 09:59:29 +0800

    feat: test extend method

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -15,7 +15,7 @@ export default class Axios {
     return this._requestMethodWithoutData('head', url, config)
   }
   options(url: string, config?: AxiosRequestConfig): AxiosPromise {
-    return this._requestMethodWithoutData('delete', url, config)
+    return this._requestMethodWithoutData('options', url, config)
   }
   post(url: string, data?: any, config?: AxiosRequestConfig) {
     return this._requestMethodWithData('post', url, data, config)
