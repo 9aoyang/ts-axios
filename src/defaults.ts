fc21cafc51ae01e582307465ed7b37ffc26bea2d
9aoyang <9aoyang@gmail.com>
2019-08-21 16:31:23 +0800

    feat: add defaults config

---
+++ src/defaults.ts
@@ -0,0 +1,29 @@
+import { AxiosRequestConfig } from './types'
+
+const defaults: AxiosRequestConfig = {
+  method: 'get',
+
+  timeout: 0,
+
+  headers: {
+    common: {
+      Accept: 'application/json, text/plain, */*'
+    }
+  }
+}
+
+const methodsData = ['delete', 'get', 'head', 'options']
+
+methodsData.forEach(method => {
+  defaults.headers[method] = {}
+})
+
+const methodsWithData = ['post', 'put', 'patch']
+
+methodsWithData.forEach(method => {
+  defaults.headers[method] = {
+    'Content-type': 'application/x-www-form-urlencoded'
+  }
+})
+
+export default defaults
