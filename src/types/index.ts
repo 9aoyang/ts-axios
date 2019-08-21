6d433f874e8460a3c28c299267a82e4a92a79739
9aoyang <9aoyang@gmail.com>
2019-08-21 10:36:16 +0800

    feat: axios overloading

--- src/types/index.ts
+++ src/types/index.ts
@@ -64,4 +64,5 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
