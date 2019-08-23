4001e4b90ab913bfa3e85c0946ac40acf166fc65
9aoyang <9aoyang@gmail.com>
2019-08-23 09:02:39 +0800

    feat: add withCredentials config

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -4,7 +4,16 @@ import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers, responseType, timeout, cancelToken } = config
+    const {
+      data = null,
+      url,
+      method = 'get',
+      headers,
+      responseType,
+      timeout,
+      cancelToken,
+      withCredentials
+    } = config
     const request = new XMLHttpRequest()
 
     if (responseType) {
@@ -14,6 +23,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     if (timeout) {
       request.timeout = timeout
     }
+
+    if (withCredentials) {
+      request.withCredentials = withCredentials
+    }
+
     request.open(method.toUpperCase(), url!, true)
 
     request.onreadystatechange = function handleLoad() {
