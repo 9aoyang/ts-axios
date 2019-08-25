79a9c96c5b194249bf6474c934dafa74e4dcaddd
9aoyang <9aoyang@gmail.com>
2019-08-25 16:35:03 +0800

    feat: validate status

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -1,26 +1,26 @@
-import axios from '../../src'
+import axios, { AxiosError } from '../../src'
 
 // document.cookie = 'a=b'
 
-axios
-  .get('/more/get')
-  .then(res => {
-    console.log(res)
-  })
-  .catch()
+// axios
+//   .get('/more/get')
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch()
 
-axios
-  .post(
-    'http://127.0.0.1:8088/more/server2',
-    {},
-    {
-      withCredentials: true
-    }
-  )
-  .then(res => {
-    console.log(res)
-  })
-  .catch()
+// axios
+//   .post(
+//     'http://127.0.0.1:8088/more/server2',
+//     {},
+//     {
+//       withCredentials: true
+//     }
+//   )
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch()
 
 // xsrf
 // const instance = axios.create({
@@ -36,20 +36,43 @@ axios
 //   .catch()
 
 // HTTP Authorization
+// axios
+//   .post(
+//     '/more/post',
+//     {
+//       a: 1
+//     },
+//     {
+//       auth: {
+//         username: 'silence1amb',
+//         password: '123456'
+//       }
+//     }
+//   )
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch()
+
+// validate status
 axios
-  .post(
-    '/more/post',
-    {
-      a: 1
-    },
-    {
-      auth: {
-        username: 'silence1amb',
-        password: '123456'
-      }
-    }
-  )
+  .get('/more/304')
   .then(res => {
     console.log(res)
   })
-  .catch()
+  .catch((e: AxiosError) => {
+    console.log(e.message)
+  })
+
+axios
+  .get('/more/304', {
+    validateStatus(status) {
+      return status >= 200 && status < 400
+    }
+  })
+  .then(res => {
+    console.log(res)
+  })
+  .catch((e: AxiosError) => {
+    console.log(e.message)
+  })
--- src/defaults.ts
+++ src/defaults.ts
@@ -27,7 +27,11 @@ const defaults: AxiosRequestConfig = {
     function(data: any): any {
       return transformResponse(data)
     }
-  ]
+  ],
+
+  validateStatus(status: number): boolean {
+    return status >= 200 && status < 300
+  }
 }
 
 const methodsData = ['delete', 'get', 'head', 'options']
